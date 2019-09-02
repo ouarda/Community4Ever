40d5f90a61bc2c812a317da00bb4803273137dfa
Xingang Liu <xingang.liu2018@outlook.com>
2019-09-02 20:13:13 +0200

    change

---
+++ Models/C4EDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Community4Ever.Models
+{
+    public class C4EDbContext : DbContext
+    {
+        public C4EDbContext(DbContextOptions<C4EDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+namespace Community4Ever.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName {get;set;}
+    }
+}
