4072fb7a34fddd3b9f089a973d8ee1c4441164b3
Xingang Liu <xingang.liu2018@outlook.com>
2019-09-02 20:14:36 +0200

    c2

--- Models/C4EDbContext.cs
+++ Models/C4EDbContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Community4Ever.Models
 {
+    
     public class C4EDbContext : DbContext
     {
         public C4EDbContext(DbContextOptions<C4EDbContext> options)
